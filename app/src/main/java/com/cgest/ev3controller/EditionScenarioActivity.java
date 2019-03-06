965c10b49b8c7a3d7ccdde513be42f3371645324
Christopher-Gst <christopher.gest1@gmail.com>
2019-03-06 11:57:10 +0100

    Interface commune des modes manuel et scan pour tablette terminée.

--- app/src/main/java/com/cgest/ev3controller/EditionScenarioActivity.java
+++ app/src/main/java/com/cgest/ev3controller/EditionScenarioActivity.java
@@ -1,6 +1,7 @@
 package com.cgest.ev3controller;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -18,7 +19,11 @@ public class EditionScenarioActivity extends AppCompatActivity {
     private final static String TAG = "EditionScenarioActivity";
 
     // Contrôles utilisateur.
+    private TextView textVEditionScenarioTitre;
+    private TextView textVActions;
+    private TextView textVEditionScenarioNomDuScenario;
 
+    private ConstraintLayout constraintLScenario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +31,27 @@ public class EditionScenarioActivity extends AppCompatActivity {
         setContentView(R.layout.layout_edition_scenario);
 
         // On récupère les vues de l'activity.
-        // - TextView "Mode automatique" en haut de l'interface.
-
-        // -  Boutons des flèches directionnelles de contrôles du robot.
+        // - TextView "Mode manuel" ou "Mode scan" en haut de l'interface.
+        textVEditionScenarioTitre = (TextView) findViewById(R.id.textVEditionScenarioTitre);
+        textVActions = (TextView) findViewById(R.id.textVActions);
+        textVEditionScenarioNomDuScenario = (TextView) findViewById(R.id.textVEditionScenarioNomDuScenario);
+        constraintLScenario = (ConstraintLayout) findViewById(R.id.constraintLScenario);
 
         // On affiche la police de caractère de l'application ("Fontdinerdotcom Huggable") sur les Views.
-        //Utile.appliquerPolicePrincipale(this, textVModeAutoTitre);
+        Utile.appliquerPolicePrincipale(this, textVEditionScenarioTitre);
+        Utile.appliquerPolicePrincipale(this, textVActions);
+        Utile.appliquerPolicePrincipale(this, textVEditionScenarioNomDuScenario);
 
+        textVEditionScenarioTitre.setText("Mode Manuel");
+        textVEditionScenarioNomDuScenario.setText("Scénario de test");
 
+        for (int i = 0; i < constraintLScenario.getChildCount(); i++) {
+            final View child = constraintLScenario.getChildAt(i);
+            if (child instanceof TextView) {
+                TextView tv = (TextView) child;
+                Utile.appliquerPolicePrincipale(this, tv);
+            }
+        }
 
     }
 }
